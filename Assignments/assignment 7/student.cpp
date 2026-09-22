fd3cf10bf038001c5d3f8580f016a6c43631ab67
Kedar Etale <kedaretale1304@gmail.com>
2026-09-22 03:48:56 +0000

    commit

---
+++ Assignments/assignment 7/student.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+class student
+{
+    int roll;
+    char name[25];
+    public:
+    void getdata()
+    {
+        cout<<"\n-------------------------";
+        cout<<"\n enter roll no. :";
+        cin>>roll;
+        cout<<"\n enter student name:";
+        cin>>name;
+    }
+    void putdata()
+    {
+        cout<<"\n------------------------";
+        cout<<"\n********student marklist********";
+        cout<<"\n-----------------------";
+        cout<<"\n roll no.:"<<roll;
+        cout<<"\n student name:"<<name<<endl;
+    }
+};
+class studentexam: public student
+{
+    public:
+    int sub1,sub2,sub3,sub4,sub5,sub6;
+    float per;
+    public:
+    void accept_data()
+    {
+        getdata();
+        cout<<"\n enter marks for subject 1:";
+        cin>>sub1;
+        cout<<"\n enter marks for subject 2:";
+        cin>>sub2;
+        cout<<"\n enter marks for subject 3:";
+        cin>>sub3;
+        cout<<"\n enter marks for subject 4:";
+        cin>>sub4;
+        cout<<"\n enter marks for subject 5:";
+        cin>>sub5;
+        cout<<"\n enter marks for subject 6:";
+        cin>>sub6;
+    }
+    void display_data()
+    {
+        putdata();
+        cout<<"\n marks of subject 1;"<<sub1;
+        cout<<"\n marks of subject 2;"<<sub2;
+        cout<<"\n marks of subject 3;"<<sub3;
+        cout<<"\n marks of subject 4;"<<sub4;
+        cout<<"\n marks of subject 5;"<<sub5;
+        cout<<"\n marks of subject 6;"<<sub6;
+    }
+};
+class studentresult: public studentexam
+
+{
+    public:
+    void calculate()
+    {
+        per=(sub1+sub2+sub3+sub4+sub5+sub6)/6.0;
+        cout<<"\n\n total percentage:"<<per;
+        cout<<"\n--------------------------\n";
+
+    }
+};
+int main()
+{studentresult str;
+    int cnt, i;
+    cout<<"\n enter no.of students you want?:";
+    cin>>cnt;
+    for(i=0;i<cnt;i++)
+    {
+        str.accept_data();
+        str.display_data();
+        str.calculate();
+
+    }
+    return 0;
+}
